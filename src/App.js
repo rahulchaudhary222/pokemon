c8d09c0460037da1cd5013e444f50b7ef06d2f1d
rahulchaudhary222 <rahulsolanki699222@gmail.com>
2022-09-27 17:16:45 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [inputval, setInputVal] = useState("");
+  const [filtered, setFiltered] = useState("");
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151%27")
+      .then((res) => res.json())
+      .then((data) => setData(data.results));
+  }, []);
+  const finder = () => {
+    let filtered = data.filter((e) => e.name.includes(inputval));
+    console.log(filtered);
+    return filtered;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+        <div className="nav-left ">
+          <input
+            placeholder="Search"
+            value={inputval}
+            onChange={(event) => {
+              setInputVal(event.target.value);
+              console.log(inputval);
+            }}
+          />
+          <button
+            onClick={() => {
+              setFiltered(filtered);
+              finder();
+            }}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+      <div className="card-wrapper">
+        {data.map((e) =>
+          filtered === e.name || !filtered ? (
+            <div className="card">
+              <h1>{e.name}</h1>
+            </div>
+          ) : null
+        )}
+      </div>
     </div>
   );
 }
